25d8c6d0561d0885482ef1fcfd324f3ab621bf9f
jodongha <66076070+jodongha@users.noreply.github.com>
2021-12-05 20:06:49 +0900

    글상세, 교환서, 마이페이지 추가

---
+++ ui/pages/changeticket.tsx
@@ -0,0 +1,110 @@
+import React from "react";
+import { useRouter } from "next/router";
+import Image from 'next/image'
+
+type PromotionPost = {
+    title: string; // 제목
+    content: string; // 내용
+    date: string; // 60%
+  };
+
+
+const PromotionPost: React.FC<{}> = () => {
+    return(
+
+
+      <div className='grid grid-cols-2 '>
+        <div className='mt-10'>
+        <div className=' box-content h-100 w-50
+        m-auto bg-white rounded-xl shadow-md overflow-hidden border-2'>
+          <div className='h-56 object-cover relative'>
+            <Image
+              src='/교환권_갤러리아포레스트_정방향.png'
+              layout='fill'
+            />
+          </div>
+          <div className='flex'>
+          <div className='p-6'>
+            <div
+              className='block mt-1 text-lg leading-tight font-medium text-black'
+            >
+              갤러리아 포레스트 이용권
+              <div className='flex flex-row mt-5 text-gray-700 '>
+                <div className='w-12 h-12 object-cover relative '>
+                    <Image
+                    src='/ticket.png'
+                    layout='fill'
+                    />
+                </div>
+                <div className='mt-6'>
+                    x8
+                </div>
+                <div className='text-xl text-center'>
+                   재고량 50
+                </div>
+            </div>
+            </div>
+            </div>
+          </div>
+          </div>
+          </div>
+    
+        <div className='mt-10'>
+        <div className='box-content h-100 w-50
+        m-auto bg-white rounded-xl shadow-md overflow-hidden border-2'>
+        <div className='h-56 object-cover relative'>
+            <Image
+              src='/교환권_갤러리아포레스트_정방향.png'
+              layout='fill'
+            />
+          </div>
+          <div className='flex'>
+          <div className='p-6'>
+            <div
+              className='block mt-1 text-lg leading-tight font-medium text-black'
+            >
+              갤러리아 포레스트 이용권
+            </div>
+            <div className='flex flex-row mt-5 text-gray-700 '>
+                <div className='w-12 h-12 object-cover relative '>
+                    <Image
+                    src='/ticket.png'
+                    layout='fill'
+                    />
+                </div>
+                <div className='mt-6'>
+                    x8
+                </div>
+                <div className='text-xl text-center'>
+                   재고량 50
+                </div>
+            </div>
+            
+
+        </div>
+        </div>
+        </div>
+        </div>
+    </div>
+
+    );
+};
+
+const ChangeTicket: React.FC<{}> = () => {
+  const router = useRouter();
+ 
+  
+  return (
+    <div>
+      <div className="mt-10 text-2xl mx-4 font-light">교환소</div>
+    <PromotionPost></PromotionPost>
+    <PromotionPost></PromotionPost>
+    <PromotionPost></PromotionPost>
+    
+
+  </div>
+
+  );
+};
+
+export default ChangeTicket;
--- ui/pages/posts/[id].tsx
+++ ui/pages/posts/[id].tsx
@@ -1,9 +1,105 @@
 import { useRouter } from "next/router";
-import React from "react";
+import React, {useState, ReactElement, useEffect} from "react";
+import { TodayWeatherData, WeatherProps } from "../../test/TodayWeaherData";
+import WeatherIcon from "../../components/WeatherIcon";
 
 const Post: React.FC<{}> = () => {
   const router = useRouter();
-  return <div>{router.query.id}</div>;
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [todayWeather, setTodayWeaher] = useState<WeatherProps>();
+  const [weatherElement, setWeatherElement] = useState<ReactElement>();
+  useEffect(() => {
+    if (router.isReady || !isLoading) {
+      // weather, fetch
+      setTodayWeaher(TodayWeatherData);
+
+      let weather: ReactElement = (
+        <div className="z-20 flex flex-col items-center">
+          <div className="w-20 h-20">
+            <WeatherIcon icon={TodayWeatherData.kind} />
+          </div>
+          <div className="flex flex-row w-full justify-around">
+            <div>
+              <span className="text-sm">습도: {TodayWeatherData.humidity}</span>
+            </div>
+            <div>
+              <span className="text-base">{TodayWeatherData.temp}도</span>
+            </div>
+          </div>
+          <div className="text-lg">{TodayWeatherData.address}</div>
+        </div>
+      );
+
+      setWeatherElement(weather);
+
+      setIsLoading(true);
+    }
+  });
+
+  return  <div className="p-8"> 👔<div className="w-full flex flex-col mt-5 h-16 justify-center border-2 shadow rounded-md ">
+  <div className="flex flex-row justify-around w-full items-center">
+
+    <div className="flex w-80  ml-3"></div>
+    <div className="flex  -mr-1 text-xl " style={{ width: "600%" }}>
+      {/* {props.post_title} */}오늘은 코트가 딱이네요
+    </div>
+    <div className="flex flex-row items-center w-full">
+      <img className="w-3 h-3 mr-2" src="/like.png" />
+      <span className="text-xs">
+        {/* {props.post_like_count} */} 67
+        </span>
+    </div>
+
+    <div className="flex flex-row items-center w-full">
+      <img className="w-3 h-3 mr-1" src="/usefull.png" />
+      <span className="text-xs">
+        {/* {props.post_useful_count} */} 22
+      </span>
+    </div>
+
+    <div className="flex flex-row items-center w-full">
+      <img className="w-3 h-3 mr-1" src="/사용자2.png" />
+      <span className="text-xs">
+        {/* {props.post_lookup_count} */} 582
+      </span>
+    </div>
+
+    <hr />
+  </div>
+  
+</div>
+<div className="w-full flex flex-row mt-5 h-40 justify-between ">
+  <div className="absoulte flex flex-col ml-5">
+          {isLoading ? <>{weatherElement}</> : <></>}
+  </div>
+  <div className="w-70 flex flex-col mt-5  h-20 justify-center">
+    <div className="flex flex-col mt-2 ml-3 text-xs">
+      기본태그
+    </div>
+    <div className="flex flex-col  p-2 text-xl">
+      #코트 #맨투맨 
+    </div>
+    <div className="flex flex-col mt-2 ml-3 text-xs">
+      사용자태그
+    </div>
+    <div className="flex flex-col  p-2 text-xl">
+      #겨울 #추워 #ootd
+    </div>
+  </div>
+  
+</div>
+
+<div className="w-full flex flex-col  border-2 shadow rounded-md  mt-5  ">
+  <div><img src="/옷사진.jpg"  width="100%"className="p-5 align-center" ></img>
+    </div>
+    <div className="p-5">
+    오늘 코트 입고 나왔는데 딱 좋네요
+  다들 감기 조심하세요!    
+    </div>
+  
+</div>
+</div>
+   
 };
 
 export default Post;
